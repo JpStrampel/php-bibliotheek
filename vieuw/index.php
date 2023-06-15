458c0ae0de630a9dadc00cac731eff9df1b73a24
Jacobdb18 <jacobdeboer42@gmail.com>
2023-06-15 11:46:02 +0200

    login in index

--- vieuw/index.php
+++ vieuw/index.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<title>Bibliotheek Applicatie</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<p> wat si dit</p>
+     <form action="login.php" method="post">
+     	<h2>Bibliotheek Applicatie Login</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+     	<label for="Gebruikersnaam">Gebruikersnaam:</label>
+     	<input type="text" name="uname" id="Gebruikersnaam" required><br>
+     	<label>Wachtwoord</label>
+     	<input type="password" name="password"  required><br>
+
+     	<button type="submit">Login</button>
+     </form>
 </body>
 </html>
