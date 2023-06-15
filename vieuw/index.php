49d8c6edd745d226b5ef711b36ea96081cbd1d55
Jacobdb18 <jacobdeboer42@gmail.com>
2023-06-15 14:27:58 +0200

    kanker

--- vieuw/index.php
+++ vieuw/index.php
@@ -7,7 +7,7 @@
 	<link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-     <form action="login.php" method="post">
+     <form action="controll/login.php" method="post">
      	<h2>Bibliotheek Applicatie Login</h2>
      	<?php if (isset($_GET['error'])) { ?>
      		<p class="error"><?php echo $_GET['error']; ?></p>
