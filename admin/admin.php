7a728f768b57a066af7bdfc86fe8046294543cce
Jacobdb18 <jacobdeboer42@gmail.com>
2023-06-19 10:56:46 +0200

    zsgezaf

--- admin/admin.php
+++ admin/admin.php
@@ -4,12 +4,15 @@ session_start();
 // Controleer of de gebruikersnaam is opgeslagen in de sessievariabele
 if(isset($_SESSION['user_name'])) {
     $gebruikersnaam = $_SESSION['user_name'];
+
     
 } else {
     // Gebruikersnaam niet gevonden, stuur door naar de inlogpagina
     header("Location: ../vieuw/index.php");
     exit();
 }
+
+
 
 
 ?>
